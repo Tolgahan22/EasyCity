3af847a8633b453864857933d89b5ee784144d8d
Tolgahan Sönmez <tolgahansonmez@outlook.com.tr>
2023-03-23 18:25:58 +0300

    Eq api Done

--- EasyCity/EqView.swift
+++ EasyCity/EqView.swift
@@ -10,28 +10,21 @@ import UIKit
 class EqView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tv: UITableView!
-    
-    var sehir1 = ""
     var sehirTv = [String]()
     var tarihTv = [String]()
     var saatTv = [String]()
-    var siddetTv = [String]()
-    var derinlikTv = [String]()
-    var latTv = [String]()
-    var lngTv = [String]()
+    var siddetTv = [Double]()
     
-    var array = [String]()
-    var contents = String()
-    var dictArray = [String]()
-    var keyArray = ["tarih", "saat", "enlem", "boylam", "derinlik", "bos1", "siddet", "bos2", "gereksiz", "sehir", "durum"]
-    var fullStack = [String : String]()
-    var allData = [[String : String]]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tv.delegate = self
         tv.dataSource = self
+        DispatchQueue.main.async {
+            self.getEqData()
+        }
+        
         
         
         
@@ -40,17 +33,65 @@ class EqView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sehirTv.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EqTableCell
-        cell.sehir.text = sehirTv[indexPath.row]
-        cell.saat.text = saatTv[indexPath.row]
-        cell.tarih.text = tarihTv[indexPath.row]
-        cell.siddet.text = siddetTv[indexPath.row]
+        cell.sehir.text = self.sehirTv[indexPath.row]
+        cell.tarih.text = self.tarihTv[indexPath.row]
+        cell.siddet.text = "\(self.siddetTv[indexPath.row])"
+        
+        
         return cell
         
     }
+    func getEqData(){
+        let url = URL(string: "https://api.orhanaydogdu.com.tr/deprem/kandilli/live")
+    
+            let session = URLSession.shared
+            let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil {
+                print(error!)
+                } else {
+                    if data != nil {
+                        do {
+                            let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String : Any]
+
+                            DispatchQueue.main.async {
+                                //print(jsonResponse?["result"]
+                                if let data = jsonResponse!["result"] as? [Any]{
+                                    // print(data)
+                                    for i in Range(0...20){
+                                        if let yer = data[i] as? [String:Any]{
+
+                                            if let title = yer["title"] as? String{
+                                                self.sehirTv.append(title)
+                                                
+                                
+                                            }
+                                            if let date = yer["date"] as? String{
+                                                self.tarihTv.append(date)
+                                            }
+                                            if let mag = yer["mag"] as? Double{
+                                                self.siddetTv.append(mag)
+                                  }
+                                  
+                                  }
+                              
+                          }
+
+                     }
+                      
+                      self.tv.reloadData()
+                  }
+              } catch {
+
+              }
+          }
+      }
+  }
+  task.resume()
+}
     
 }
-
